f3ff8c215465008e0b729c7ba63b989b1b0ba56c
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-02-16 03:23:51 +0530

    Added flex grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,8 +7,22 @@ var TicTacToe = (function($) {
     _o_score = 0,
     _valid_matches = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
         [2, 5, 8], [0, 4, 8], [2, 4, 6]],
-    _current_player, // can be "X" or "O"
-    $reset_button, $x_score, $o_score, $cards, round_count;
+    _current_player = "X", // can be "X" or "O"
+    $reset_button, 
+    $x_score, 
+    $o_score, 
+    $cards, 
+    $message, 
+    $score_display, 
+    _messages = [
+      "Start game or select a player to start with", 
+      " Wins!!", 
+      "You have selected ",
+      " 's turn",
+      "That's a draw!!"
+      ],
+    _btns_text = ["Reset Game?", "Rematch"],
+    _match_end;
 
   _init();
 
@@ -16,13 +30,7 @@ var TicTacToe = (function($) {
     _cacheDom();
     resetBoard();
     _bindEvents();
-  }
-
-  function resetBoard() {
-    _x_clicked_index = [];
-    _o_clicked_index = [];
-    $cards.html("").removeClass("matched");
-    _current_player = "X"
+    displayScores();
   }
 
   function _cacheDom() {
@@ -30,21 +38,43 @@ var TicTacToe = (function($) {
     $x_score = $("#x_score");
     $o_score = $("#o_score");
     $cards = $(".card");
+    $message = $(".message");
+    $score_display = $(".score_display");
   }
 
   function _bindEvents() {
     $cards.on("click", _playGame);
     $reset_button.on("click", resetBoard);
+    $score_display.on("click", changePlayer);
+  }
+
+  function resetBoard(e, playerChanged) {
+    _x_clicked_index = [];
+    _o_clicked_index = [];
+    _match_end = false;
+    $cards.html("").removeClass("matched");
+    if(_current_player === "X") {
+      $score_display[0].classList.add("selected-player");
+      $score_display[1].classList.remove("selected-player");
+    } else {
+      $score_display[1].classList.add("selected-player");
+      $score_display[0].classList.remove("selected-player");
+    }
+    $reset_button.html(_btns_text[0]);
+    if(playerChanged){
+      $message.html(_messages[2] + "<b>" + _current_player + "</b>");  
+    } else {
+      $message.html(_messages[0]);
+    }
   }
 
   function _playGame(event) {
-    event.preventDefault();
-    var $target = $(event.target);
+    var $this = $(this);
 
-    if ($target.html() === "") {
+    if ($this.html() === "" && !_match_end) {
 
-      var index = $target.index(".card");
-      $target.html(_current_player);
+      var index = $this.index(".card");
+      $this.html(_current_player);
 
       _current_player === "X" ? _x_clicked_index.push(index) : _o_clicked_index.push(index);
 
@@ -56,25 +86,33 @@ var TicTacToe = (function($) {
           _current_player === "X" ? ++_x_score : ++_o_score;
           $(".card" + box_selected.matchedPair[0] + ", .card" + box_selected.matchedPair[1] + ", .card" + box_selected.matchedPair[2]).addClass("matched");
           displayScores();
-          if (confirm(_current_player + " Wins!!\nDo you want to play the next round?")) {
-            resetBoard();
-          }
+          $reset_button.html(_btns_text[1]);
+          $message.html("<b>" + _current_player + "</b>" + _messages[1]);
+          _match_end = true;
           return;
         }
 
       }
 
-      _current_player === "X" ? _current_player = "O" : _current_player = "X";
-
+      _current_player = _current_player === "X" ?  "O" : "X";
+      $message.html("<b>" + _current_player + "</b>" + _messages[3]);
+      $score_display.toggleClass("selected-player");
     }
 
     if ((_x_clicked_index.length + _o_clicked_index.length) === 9) {
-      if (confirm("That's a draw!!\nDo you want to play the next round?")) {
-        resetBoard();
-      }
+      $reset_button.html(_btns_text[1]);
+      $message.html(_messages[4]);
       return;
     }
+    event.preventDefault();
+    event.stopPropagation();
+  }
 
+  function changePlayer() {
+    var idx = $(this).index();
+    _current_player = idx === 0 ? "X" : "O";
+    $message.html(_messages[2] + "<b>" + _current_player + "</b>");
+    resetBoard({}, true);
   }
 
   function checkIfMatchedFor(current_player) {
@@ -101,8 +139,8 @@ var TicTacToe = (function($) {
   }
 
   function displayScores() {
-    $x_score.html(_x_score);
-    $o_score.html(_o_score);
+    $x_score.html(_x_score ? _x_score : "-");
+    $o_score.html(_o_score ? _o_score : "-");
   }
 
   return {
